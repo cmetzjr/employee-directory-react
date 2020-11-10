c7e54031167ec0bc1f82818a69596cc2d762784f
Charles Metzger <cmetzjr@yahoo.com>
2020-11-10 00:23:05 -0500

    update handleInputChange

--- src/components/EmpContainer.js
+++ src/components/EmpContainer.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import API from "../utils/API";
 
 class EmpContainer extends Component {
-    //set initial state
+    //set initial state for 2 arrays
     state = {
-        employees: []
+        employees: [],
+        empFiltered: []
     };
 
    
-    //API call for random user data, take the employee data that I want, and set state
+    //API call for random user data, map the employee data that I want, and set state for both arrays
     getEmployees = async () => {
         try {
             const res = await API.getDummyEmp();
@@ -20,23 +21,22 @@ class EmpContainer extends Component {
                 email: emp.email,
                 id: emp.id.value                 
             }))
-            this.setState({ employees: empInfo})
+            this.setState({ employees: empInfo, empFiltered: empInfo })
         } catch (err){
             console.log(err)
         };
     };
 
     
-    //get random employee data
+    //call getEmployees
     componentDidMount() {
         this.getEmployees();
     }
 
-
+    //this function will take the search parameters and will update the state of the empFiltered array based on filtering of the employees array
     handleInputChange = (value) => {
-        console.log(value);
         this.setState({
-            employees: this.state.employees.filter(x => x.last.includes(value))
+            empFiltered: this.state.employees.filter(x => x.last.includes(value))
         });
     }
 
@@ -46,6 +46,7 @@ class EmpContainer extends Component {
                 <div className="mb-4">
                     <h1 className="text-center mb-4">Employee Directory</h1>
                     <label className="mr-4" htmlFor="text">Search for employees:</label>
+                    {/* letters typed into the input box (=value) become the parameter of handleInputChange */}
                     <input type="text" onInput={event => this.handleInputChange(event.target.value)} />
                 </div>
             
@@ -59,8 +60,9 @@ class EmpContainer extends Component {
                         <th scope="col">Email</th>
                     </tr>
                 </thead>
-                <tbody>
-                    {this.state.employees.map(empInfo=>
+                <tbody> 
+                    {/* maps the empFiltered array and renders it to the table */}
+                    {this.state.empFiltered.map(empInfo=>
                     <tr key={empInfo.id}>                            
                         <td><img src={empInfo.pic} alt="employee headshot"></img></td>
                         <td>{empInfo.last}</td>
