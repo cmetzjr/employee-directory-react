d00cfe8106a00eab32a0d0b2c2efce492f82a511
Charles Metzger <cmetzjr@yahoo.com>
2020-11-12 23:50:40 -0500

    update API to increase returned objects

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASEURL = "https://randomuser.me/api/?results=10&nat=us";
-// const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+// const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=75";
 
 // get dummy employees
 export default {
